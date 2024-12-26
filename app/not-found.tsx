09048522906c58c2f1517160f95c0f4d9c4dd749
Rafael Coelho <coelhorafael8@gmail.com>
2024-12-25 21:55:40 -0300

    bugfix

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+      <main className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold mb-4">Página não encontrada</h1>
+        <p className="text-gray-600">
+          A página que você está procurando não existe ou foi movida.
+        </p>
+      </main>
+    )
+  }
