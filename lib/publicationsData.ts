a05f9b2c7ab1ac7e48d91566e7d299ddeff305ee
Rafael Coelho <coelhorafael8@gmail.com>
2025-01-01 23:54:38 -0300

    reverting

--- lib/publicationsData.ts
+++ lib/publicationsData.ts
@@ -28,7 +28,7 @@ export const publications: Publication[] = [
   { id: 2, title: "Estudo Especial: Economia Verde", category: "ESTUDOS ESPECIAIS", year: 2023, semester: "First" },
   { id: 3, title: "Nota Técnica: Análise da Inflação", category: "NOTAS TÉCNICAS", year: 2023, semester: "Second" },
   { id: 4, title: "Impacto Econômico do Carnaval", category: "CARNAVAL DE DADOS", year: 2022, semester: "First" },
-  { id: 5, title: "Dados de Turismo do Réveillon", category: "RÉVEILLON EM DADOS", year: 2022, semester: "Second" },
+  { id: 5, title: "Dados do Turismo do Réveillon", category: "RÉVEILLON EM DADOS", year: 2022, semester: "Second" },
   // Add more publications as needed
 ];
 
